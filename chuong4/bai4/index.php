e163e00956f0bc55748decdf0299d63cc1a093d7
nguyentu9 <huynhthanhtoan517@gmail.com>
2020-12-14 10:59:34 +0700

    update

--- chuong4/bai4/index.php
+++ chuong4/bai4/index.php
@@ -7,7 +7,22 @@
 	<title>Bài 4</title>
 </head>
 <body>
-	<div class="container">
+	<?php
+		$tendangnhap = $matkhau = $hoten = $ngaysinh = $gioitinh = $email = $diachi = $sdt = '';
+		if(isset($_POST['dangky'])){
+			if(isset($_POST['hoten'])) $hoten = $_POST['hoten'];
+			if(isset($_POST['tendangnhap'])) $tendangnhap = $_POST['tendangnhap'];
+			if(isset($_POST['matkhau'])) $matkhau = $_POST['matkhau'];
+			if(isset($_POST['ngaysinh'])) $ngaysinh = $_POST['ngaysinh'];
+			$gioitinh = isset($_POST['gioitinh']) ? 'Nam' : 'Nữ';
+			if(isset($_POST['dienthoai'])) $sdt = $_POST['dienthoai'];
+			if(isset($_POST['diachi'])) $diachi = $_POST['diachi'];
+			if(isset($_POST['email'])) $email = $_POST['email'];
+		}
+	?>
+
+
+	<form action="" class="container" method="POST">
 		<header>
 			<p>Hồ sơ đăng ký</p>
 		</header>
@@ -30,13 +45,16 @@
 			<p id="_hoten">Họ tên khách hàng</p>
 			<input type="text" id="hoten" name="hoten">
 			<p id="_ngaysinh">Ngày sinh</p>
-			<input type="date" value="2000-01-01">
+			<input type="date" value="2000-01-01" name='ngaysinh'>
 			<p id="_email">Email</p>
-			<input type="email" id="email" name="">
+			<input type="email" id="email" name="email">
 			<p id="_thunhap">Thu nhập</p>
 			<input type="number" id="thunhap" name="thunhap">
 			<p id="_gioitinh">Giới tính</p>
-			<input type="checkbox" id="gioitinh" name="gioitinh">
+			<div>
+				<label for='gioitinh'>Nam</span>
+				<input type="checkbox" id="gioitinh" name="gioitinh">
+			</div> 
 			<p id="_diachi">Địa chỉ</p>
 			<textarea id="diachi" name="diachi"></textarea>
 			<p id="_dienthoai">Điện thoại</p>
@@ -49,13 +67,19 @@
 		</div>
 		<div class="info_customer_detail">
 			<ul>
-				<li>lorem</li>
-				<li>lorem</li>
-				<li>lorem</li>
-				<li>lorem</li>
-				<li>lorem</li>
+				<?php
+					echo  
+					"<li>Họ tên khách hàng: $hoten</li>
+					<li>Tên đăng nhập: $tendangnhap</li>
+					<li>Mật khẩu: $matkhau</li>
+					<li>Ngày sinh:$ngaysinh</li>
+					<li>Giới tính: $gioitinh</li>
+					<li>Email:$email</li>
+					<li>Địa chỉ:$email </li>
+					<li>Điện thoại:$sdt</li>";
+				?>
 			</ul>
 		</div>
-	</div>
+	</form>
 </body>
 </html>
